9ff6728868f11ea47a0c2177d005566c9bb58332
Jorge William <jorge_william_cardoso@yahoo.com.br>
2023-01-02 14:56:46 -0300

    Feat: Modulo request criado

---
+++ request.js
@@ -0,0 +1,15 @@
+const RESQUEST_TIMEOUT = 500;
+
+function encrypt(data) {
+    return 'encripted data';
+}
+
+function send(url, data) {
+    const encryptedData = encrypt(data);
+    console.log(`sending ${encryptedData} to ${url}`);
+}
+
+module.exports = {
+    RESQUEST_TIMEOUT,
+    send
+}
