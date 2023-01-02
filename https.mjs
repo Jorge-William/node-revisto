2c24c6890e5067e0bc091ae451cb7ecc5cd870dc
Jorge William <jorge_william_cardoso@yahoo.com.br>
2023-01-02 15:19:39 -0300

    Fix: Usando modulos es6

--- https.mjs
+++ https.mjs
@@ -1,6 +1,6 @@
-const { send } = require('./request');
-const { read } = require('./response');
-const { RESQUEST_TIMEOUT } = require('./request')
+import { send } from './request.mjs';
+import { read } from './response.mjs';
+import { RESQUEST_TIMEOUT } from './request.mjs';
 
 function makeRequest (url, data){
     send(url, data);
--- request.mjs
+++ request.mjs
@@ -9,7 +9,7 @@ function send(url, data) {
     console.log(`sending ${encryptedData} to ${url}`);
 }
 
-module.exports = {
+export {
     RESQUEST_TIMEOUT,
     send
 }
