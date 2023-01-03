bb279f03934f4509f1e80358f91d392db21a9cb1
Jorge William <jorge_william_cardoso@yahoo.com.br>
2023-01-03 16:10:08 -0300

    Feat: First webserver

--- 02-http-server/index.js
+++ 02-http-server/index.js
@@ -2,13 +2,31 @@ const http =  require('http');
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-    });
-    res.end(JSON.stringify({
-        id: 1, name: 'Sir Isaac Newton'
-    }))
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    if(req.url === '/friends') {
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
+        });
+        res.end(JSON.stringify({
+            id: 1, name: 'Sir Isaac Newton'
+        }))
+    } else if(req.url === '/messages') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<ul>');
+        res.write('<li>Hello World!!</li>');
+        res.write('<li>My name is <b>Jorge William</b></li>');
+        res.write('<ul>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
 });
 
 server.listen(PORT, () => {
