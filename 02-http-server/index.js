2a1536b5d7722b0690019e5d8ed3925e2941759c
Jorge William <jorge_william_cardoso@yahoo.com.br>
2023-01-03 15:44:44 -0300

    Feat: Arquivo inicial sobre servidor http

---
+++ 02-http-server/index.js
@@ -0,0 +1,16 @@
+const http =  require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'application/json',
+    });
+    res.end(JSON.stringify({
+        id: 1, name: 'Sir Isaac Newton'
+    }))
+});
+
+server.listen(PORT, () => {
+    console.log(`Listen on port ${PORT}...` );
+}); // 127.0.0.1 => localhost
